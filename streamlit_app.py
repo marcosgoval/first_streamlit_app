016c4b8a3587d1d279c89cdf2e0f168f6eb0e766
MARCOS GARCIA MEIRELLES <49986256+marcosgoval@users.noreply.github.com>
2023-09-20 19:10:09 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,6 @@ import streamlit
 
 streamlit.title('My Parentes New Healthy Diner')
 streamlit.header('Breakfast Menu')
-streamlit.text('')
-streamlit.text('')
-streamlit.text('')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boliled FreeRange Egg')
